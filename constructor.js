c0d12f2b5fde94452981cf3f44096b2e2ccaad56
Rohit Sinha <rohitsinha4826@gmail.com>
2018-06-06 16:16:53 +0530

    First constructor commit

---
+++ constructor.js
@@ -0,0 +1,17 @@
+class Person{
+    constructor(name, noOfYears, print){
+        this.name=name;
+        this.age = noOfYears;
+        this.print = print
+    }
+    printMyName(){
+        console.log(this.name+ " Aged "+ this.age);
+    }
+}
+
+const person1=new Person("rohit", 24, function(){console.log(" Namaskar I am "+ this.name)});
+
+const person2 = new Person("Mohit ", 32, function(){console.log("Hello I am "+ this.name )})
+
+person2.print()
+person1.print()
